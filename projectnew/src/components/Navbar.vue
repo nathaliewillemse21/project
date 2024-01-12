33c374859e97f19fcc1bf302cceb22012b545a35
nathaliewillemse21 <njpwillemse69@gmail.com>
2024-01-12 11:25:41 +0200

    navbar

---
+++ projectnew/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <router-link to="/">
+        <img
+          class="img-fluid"
+          src="https://i.ibb.co/tbQynkf/NW.png"
+          loading="lazy"
+          alt="logo"
+        />
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link active">Home | </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">About | </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/resume">Resume | </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/testimonials"
+              >Testimonials |
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/project">Project | </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contact">Contact | </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style scoped></style>
---
+++ projectnew/src/components/SpinnerView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-center">
+      <div class="spinner-border" v-if="isLoading" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SpinnerView',
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  method: {
+    fetchData() {
+      this.isloading = true;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 2000);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
